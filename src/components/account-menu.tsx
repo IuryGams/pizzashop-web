0e2c8fb8ff5c6bcb3e11aaeafa8d4b733cacf101
Iury Gama <Iuryempregos@gmail.com>
2025-03-11 19:52:53 -0300

    create Header Component

---
+++ src/components/account-menu.tsx
@@ -0,0 +1,32 @@
+import { Building, ChevronDown, LogOut } from "lucide-react";
+import { Button } from "./ui/button";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from "./ui/dropdown-menu";
+
+
+export function AccountMenu() {
+    return (
+        <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+                <Button variant={"outline"} className="flex items-center gap-2 select-none cursor-pointer rounded" >
+                    Pizza Shop
+                    <ChevronDown />
+                </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end" className="w-56">
+                <DropdownMenuLabel className="flex flex-col">
+                    <span>Iury Menezes</span>
+                    <span className="text-xs font-normal text-muted-foreground" >iurydev7@gmail.com</span>
+                </DropdownMenuLabel>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem className="cursor-pointer">
+                    <Building className="w-4 h-4"/>
+                    <span>Perfil da loja</span>
+                </DropdownMenuItem>
+                <DropdownMenuItem className="cursor-pointer text-rose-500 dark:text-rose-400">
+                    <LogOut className="w-4 h-4"/>
+                    <span>Sair</span>
+                </DropdownMenuItem>
+            </DropdownMenuContent>
+        </DropdownMenu>
+    )
+}
